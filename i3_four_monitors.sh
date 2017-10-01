71fb17bfca6af96397e029620d516e372d7a0d89
spoonisnull <spoonisnull@gmail.com>
2017-10-01 02:58:31 +0100

    updated config/dotfiles

---
+++ i3_four_monitors.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1-3 --off --output DVI-D-0 --mode 1440x900 --pos 1600x0 --rotate left --output DVI-D-1 --off --output HDMI-1-1 --off --output HDMI-1-2 --off --output DP-1-1 --mode 1600x900 --pos 0x540 --rotate normal --output HDMI-0 --mode 1280x1024 --pos 4100x416 --rotate normal --output DP-1 --primary --mode 1600x900 --pos 2500x540 --rotate normal --output DP-0 --off
